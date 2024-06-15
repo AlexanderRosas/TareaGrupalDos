9b783892015f977f294bf14cd9a879e09658ca0e
Marckr02 <marco.rios@epn.edu.ec>
2024-06-15 01:29:36 -0500

    Se agregó nuevos métodos rmC03, rmC05, rmC07, rmA04, rmL02, rmL08 y rmR02 a la clase RiosMarco

--- src/RiosMarco.java
+++ src/RiosMarco.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class RiosMarco {
+    private Scanner scanner;
 
-    static Scanner sc = new Scanner(System.in);
+    public RiosMarco() {
+        this.scanner = new Scanner(System.in);
+    }
 
     public void rmSN3(int n) {
         int a = 0, b = 1, c;
@@ -135,5 +138,198 @@ public class RiosMarco {
             System.out.println("2");
         }
     }
+
+    public void rmC03() {
+        System.out.println("Ingrese una frase para eliminar una vocal:");
+        String frase = scanner.nextLine();
+        System.out.println("Ingrese una vocal para eliminar en rmC03:");
+        char vocal = scanner.nextLine().charAt(0);
+        String resultado = eliminarVocal(frase, vocal);
+        System.out.println("Resultado rmC03: " + resultado);
+    }
+
+    private String eliminarVocal(String frase, char vocal) {
+        char lowerVocal = Character.toLowerCase(vocal);
+        char upperVocal = Character.toUpperCase(vocal);
+        
+        StringBuilder resultado = new StringBuilder();
+        
+        for (char c : frase.toCharArray()) {
+            if (c != lowerVocal && c != upperVocal) {
+                resultado.append(c);
+            }
+        }
+        
+        return resultado.toString();
+    }
+
+    public void rmC05() {
+        System.out.println("\nIngrese una frase para invertirla :");
+        String frase = scanner.nextLine();
+        String resultado = invertirYMayusculas(frase);
+        System.out.println("Resultado rmC05: " + resultado);
+    }
+
+    private String invertirYMayusculas(String frase) {
+        StringBuilder resultado = new StringBuilder();
+        
+        for (int i = frase.length() - 1; i >= 0; i--) {
+            char c = frase.charAt(i);
+            if (esVocal(c)) {
+                resultado.append(Character.toUpperCase(c));
+            } else {
+                resultado.append(c);
+            }
+        }
+        
+        return resultado.toString();
+    }
+
+    private boolean esVocal(char c) {
+        char lowerC = Character.toLowerCase(c);
+        return lowerC == 'a' || lowerC == 'e' || lowerC == 'i' || lowerC == 'o' || lowerC == 'u';
+    }
+
+    public void rmC07() {
+        System.out.println("\nIngrese una frase para rmC07:");
+        String frase = scanner.nextLine();
+        String resultado = mayusculasSinJ(frase);
+        System.out.println("Resultado rmC07: " + resultado);
+    }
+
+    private String mayusculasSinJ(String frase) {
+        String upperCaseFrase = frase.toUpperCase();
+        
+        StringBuilder resultado = new StringBuilder();
+        
+        for (char c : upperCaseFrase.toCharArray()) {
+            if (c != 'J') {
+                resultado.append(c);
+            }
+        }
+        
+        return resultado.toString();
+    }
+
+    public void rmA04() {
+        System.out.println("\nIngrese su nombre:");
+        String nombre = scanner.nextLine();
+        System.out.println("Ingrese su apellido:");
+        String apellido = scanner.nextLine();
+        crearMatrizX(nombre, apellido);
+    }
+
+    private void crearMatrizX(String nombre, String apellido) {
+        int lenNombre = nombre.length();
+        int lenApellido = apellido.length();
+        int n = Math.max(lenNombre, lenApellido);
+        char[][] matriz = new char[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            if (i < lenNombre) {
+                matriz[i][i] = nombre.charAt(i);
+            }
+            if (i < lenApellido) {
+                matriz[i+1][n - i - 1] = apellido.charAt(i);
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matriz[i][j] == 0) {
+                    System.out.print("   "); 
+                } else {
+                    System.out.print(matriz[i][j] + "  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void rmL02() {
+        System.out.println("\nIngrese un caracter para la carga en mrL02:");
+        char caracter = scanner.nextLine().charAt(0);
+    
+        System.out.print("Cargando: ");
+        for (int i = 0; i <= 20; i++) {
+            System.out.print("\r"); 
+    
+            StringBuilder barra = new StringBuilder("[");
+            for (int j = 0; j < i; j++) {
+                barra.append(caracter);
+            }
+            for (int j = i; j < 20; j++) {
+                barra.append(" ");
+            }
+            barra.append("] " + (i * 5) + "%");
+    
+            System.out.print(barra.toString());
+    
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println();
+    }
+    
+
+    public void rmL08() {
+        System.out.println("\nIngrese su nombre y apellidos para la carga en mrL08:");
+        String nombreCompleto = scanner.nextLine();
+    
+        int totalCaracteres = nombreCompleto.length();
+    
+        System.out.print("Cargando: ");
+        for (int i = 0; i <= totalCaracteres; i++) {
+            System.out.print("\r"); 
+    
+            StringBuilder barra = new StringBuilder("[");
+            for (int j = 0; j < i; j++) {
+                barra.append(nombreCompleto.charAt(j));
+            }
+            for (int j = i; j < totalCaracteres; j++) {
+                barra.append(" ");
+            }
+            int porcentaje = (int) ((i / (double) totalCaracteres) * 100);
+            barra.append("] " + porcentaje + "%");
+    
+            System.out.print(barra.toString()); 
+    
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println();
+    }
+
+    public static void rmR02() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número (a):");
+        int a = scanner.nextInt();
+
+        System.out.println("Ingrese el segundo número (b):");
+        int b = scanner.nextInt();
+
+        int resultado = sumaRecursiva(a, b, 0);
+
+        System.out.println("La suma de " + a + " y " + b + " es: " + resultado);
+
+        scanner.close();
+    }
+
+    private static int sumaRecursiva(int a, int b, int acumulado) {
+ 
+        if (b == 0) {
+            return acumulado + a;
+        }
+
+        return sumaRecursiva(a, b - 1, acumulado + 1);
+    }
+
 
 }
